e9df9febb8d4f5a0b869d2ab2226631fde118417
WillianYamakawa <84150755+WillianYamakawa@users.noreply.github.com>
2023-11-25 00:29:01 -0300

    fixes

--- Controllers/DeviceController.cs
+++ Controllers/DeviceController.cs
@@ -34,7 +34,7 @@ public class DeviceController : ControllerBase{
 
     [HttpPost]
     public ActionResult Post([FromBody] DeviceRequest payload){
-        //if(AuthGetter.Get(Request, _dbContext) == null) return BadRequest(ResultObject.BuildUnauthenticated());
+        if(AuthGetter.Get(Request, _dbContext) == null) return BadRequest(ResultObject.BuildUnauthenticated());
         if(string.IsNullOrWhiteSpace(payload.Token)) return BadRequest(ResultObject.BuildErrors("Campos inválidos!"));
         Device device = new Device() { Token = payload.Token, OwnerID = payload.OwnerId};
         return device.Save(_dbContext) ? Ok(ResultObject.Build("Dispositivo criado com sucesso!")) : StatusCode(StatusCodes.Status500InternalServerError, ResultObject.BuildErrors("Erro interno"));
@@ -42,7 +42,7 @@ public class DeviceController : ControllerBase{
 
     [HttpGet]
     public ActionResult Get([FromQuery] int id){
-        //if(AuthGetter.Get(Request, _dbContext) == null) return BadRequest(ResultObject.BuildUnauthenticated());
+        if(AuthGetter.Get(Request, _dbContext) == null) return BadRequest(ResultObject.BuildUnauthenticated());
         Device? record = Device.GetByID(id, _dbContext);
         if(record == null) return BadRequest(ResultObject.BuildErrors("Dispositivo inexistente!"));
         return Ok(ResultObject.Build(record));
@@ -50,7 +50,7 @@ public class DeviceController : ControllerBase{
 
     [HttpGet("all")]
     public ActionResult List([FromQuery] QueryFiltersDevices filter){
-        //if(AuthGetter.Get(Request, _dbContext) == null) return BadRequest(ResultObject.BuildUnauthenticated());
+        if(AuthGetter.Get(Request, _dbContext) == null) return BadRequest(ResultObject.BuildUnauthenticated());
         Device.DeviceResult[]? records = Device.GetRecords(filter.PagingStart ?? 0, filter.PagingEnd ?? 50, filter.ownerId, _dbContext);
         if(records == null) return StatusCode(StatusCodes.Status500InternalServerError, ResultObject.BuildErrors("Erro interno"));
         return Ok(ResultObject.Build(records));
